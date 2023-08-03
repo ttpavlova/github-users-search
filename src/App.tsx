3a8e91a93afcd4552328350fbea66af2f4caed7c
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2023-08-03 21:52:20 +0300

    Move part of the code to the SearchPage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
-import { useState } from "react";
-import { SearchBar } from "./components/SearchBar/SearchBar";
-import { UserList } from "./components/UserList/UserList";
-import { useUsers } from "./hooks/useUsers";
-import { useDebounce } from "./hooks/useDebounce";
-import { usePagination } from "./hooks/usePagination";
-import { Pagination } from "./components/Pagination/Pagination";
-import { Col, Layout, Row, Space } from "antd";
+import { SearchPage } from "./components/SearchPage/SearchPage";
+import { Layout } from "antd";
 import { Content, Footer, Header } from "antd/es/layout/layout";
 import "./App.scss";
 
 function App() {
-    const [query, setQuery] = useState("");
-    const [order, setOrder] = useState("desc");
-    const [loading, setLoading] = useState(false);
-
-    const { page, changePage } = usePagination();
-    const debouncedQuery = useDebounce(query, 1000);
-    const { users } = useUsers(debouncedQuery, order, page, setLoading);
-
-    const changeQuery = (e: React.FormEvent<HTMLInputElement>) => {
-        setQuery(e.currentTarget.value);
-        setLoading(true);
-    };
-
-    const changeOrder = (value: string) => {
-        setOrder(value);
-    };
-
     return (
         <div className="App">
             <Layout className="layout">
@@ -34,34 +11,7 @@ function App() {
                     <h1>Users Search</h1>
                 </Header>
                 <Content className="content">
-                    <Row>
-                        <Col md={24} lg={8}></Col>
-                        <Col md={24} lg={8}>
-                            <Space
-                                direction="vertical"
-                                size="middle"
-                                style={{
-                                    display: "flex",
-                                }}
-                            >
-                                <SearchBar
-                                    query={query}
-                                    handleChange={changeQuery}
-                                    loading={loading}
-                                />
-                                <UserList
-                                    users={users}
-                                    handleChange={changeOrder}
-                                    loading={loading}
-                                />
-                                <Pagination
-                                    handleChange={changePage}
-                                    total_count={users.total_count}
-                                />
-                            </Space>
-                        </Col>
-                        <Col md={24} lg={8}></Col>
-                    </Row>
+                    <SearchPage />
                 </Content>
                 <Footer className="footer"></Footer>
             </Layout>
---
+++ src/components/SearchPage/SearchPage.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { useUsers } from "../../hooks/useUsers";
+import { useDebounce } from "../../hooks/useDebounce";
+import { usePagination } from "../../hooks/usePagination";
+import { SearchBar } from "../SearchBar/SearchBar";
+import { UserList } from "../UserList/UserList";
+import { Pagination } from "../Pagination/Pagination";
+import { Col, Row, Space } from "antd";
+
+export const SearchPage = () => {
+    const [query, setQuery] = useState("");
+    const [order, setOrder] = useState("desc");
+    const [loading, setLoading] = useState(false);
+
+    const { page, changePage } = usePagination();
+    const debouncedQuery = useDebounce(query, 1000);
+    const { users } = useUsers(debouncedQuery, order, page, setLoading);
+
+    const changeQuery = (e: React.FormEvent<HTMLInputElement>) => {
+        setQuery(e.currentTarget.value);
+        setLoading(true);
+    };
+
+    const changeOrder = (value: string) => {
+        setOrder(value);
+    };
+
+    return (
+        <Row>
+            <Col md={24} lg={8}></Col>
+            <Col md={24} lg={8}>
+                <Space
+                    direction="vertical"
+                    size="middle"
+                    style={{
+                        display: "flex",
+                    }}
+                >
+                    <SearchBar
+                        query={query}
+                        handleChange={changeQuery}
+                        loading={loading}
+                    />
+                    <UserList
+                        users={users}
+                        handleChange={changeOrder}
+                        loading={loading}
+                    />
+                    <Pagination
+                        handleChange={changePage}
+                        total_count={users.total_count}
+                    />
+                </Space>
+            </Col>
+            <Col md={24} lg={8}></Col>
+        </Row>
+    );
+};
